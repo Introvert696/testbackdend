347041b49722a46caed475d09abb7c3cf0dae198
Ivan Filatov <introvert696@yandex.ru>
2025-02-14 11:37:07 +0300

    update doker files and create readme

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+until php bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do
+  echo "Waitting for database to be ready..."
+  sleep 2
+done
+
+php bin/console doctrine:database:create --if-not-exists
+
+php bin/console doctrine:migrations:migrate --no-interaction
+
+exec "php-fpm"
