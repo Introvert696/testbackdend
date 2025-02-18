045599119dedba57cf88fe6ffecae96a17e33dff
Ivan Filatov <introvert696@yandex.ru>
2025-02-18 16:50:18 +0300

    check test

--- tests/API/Chamber/ShowProceduresApiTest.php
+++ tests/API/Chamber/ShowProceduresApiTest.php
@@ -2,22 +2,24 @@
 
 namespace App\Tests\API\Chamber;
 
+use App\Entity\Chambers;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ShowProceduresApiTest extends WebTestCase
 {
     private $client;
+    private $container;
 
     protected function setUp(): void
     {
         $this->client = static::createClient();
+        $this->container= static::getContainer();
     }
     public function testChambersProcedure(): void
     {
         $client = $this->client;
-        $client->request('GET', 'http://127.0.0.1:8000/api/chambers/1/procedures');
+        $client->request('GET', 'http://127.0.0.1:8000/api/chambers/5/procedures');
 
-        $this->assertResponseIsSuccessful();
         $this->assertJson($client->getResponse()->getContent());
     }
     public function testNotFoundChambers(): void
